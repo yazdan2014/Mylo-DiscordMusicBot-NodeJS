93862dd1881910b7df7e1b563a2ab98ccc983729
yazdanzn <>
2021-09-26 17:10:04 +0330

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on("messageCreate", async message => {
             })
 
             break;
-        case "mame":case "play":
+        case "p":case "play":
             var query = message.content.slice(commandWithPrefix.length +1 , message.content.length)
             var channel = message.member.voice.channel
             if(!channel) return message.channel.send("Join a channel")
