4a187a3bfd3b74a53bd4b1df75ccff69d5ccddf3
yazdanzn <>
2021-10-08 21:05:42 +0330

    Update index.js

--- index.js
+++ index.js
@@ -767,7 +767,7 @@ client.on("guildCreate", guild =>{
 
 client.on('guildDelete', guild =>{
     queue.get(guild.id) = {}
-    
+    client.guilds.cache.get("896070505717727272").channels.cache.get("896070505717727278").send("just left"+ guild.name +`kose nanat` + "\nThere are currently "+ client.guilds.cache.size  + 'guilds using the coolest bot ever')
 })
 
 function secToMinSec(sec){
