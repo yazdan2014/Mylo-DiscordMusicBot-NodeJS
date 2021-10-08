6c9f34260f27a58a9153ba6f167ec3463e8edbde
yazdanzn <>
2021-10-08 22:38:04 +0330

    Update index.js

--- index.js
+++ index.js
@@ -608,13 +608,13 @@ client.on("messageCreate", async message => {
             var outPut = ""
 
             guildQueue.forEach(function(resource,index) {
-                if(index == 0)return outPut += ":arrow_forward: Now playing " + resource.metadata.title + "\n"
-                outPut +=  index + ". " + resource.metadata.title + "\n"
+                if(index == 0)return outPut += "▶ Now playing `" + resource.metadata.title + "`\n"
+                outPut +=  index + ". `" + resource.metadata.title + "`\n"
             })
 
             var embed = new MessageEmbed()
-            .setTitle(":music:Queue Review")
-            .setDescription("```"+ outPut +"```")
+            .setTitle("<:music:896110571206869042> Queue Review")
+            .setDescription( outPut)
             .setFooter("requested by:" + message.author.username, message.author.avatarURL())
             
 
