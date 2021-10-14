e5137351ff57af225ce14d4128056b5f72bee70a
yazdanzn <>
2021-10-14 13:50:52 +0330

    Update index.js

--- index.js
+++ index.js
@@ -836,7 +836,7 @@ client.on("voiceStateUpdate" , (oldState , newState)=>{
             queue.get(oldState.guild.id).audioPlayer.stop(true);
             queue.get(oldState.guild.id).resources = []
         }catch{}
-    } , 10_000)
+    } , 40_000)
 
     let interval = setInterval(function(){
         if(player.state.status == AudioPlayerStatus.Idle){
