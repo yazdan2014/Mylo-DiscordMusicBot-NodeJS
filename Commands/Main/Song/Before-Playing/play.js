56b404cf8dc3e59b92bb43c9d0d0bc75cd71de6b
yazdanzn <>
2021-10-19 17:37:34 +0330

    ASD

--- Commands/Main/Song/Before-Playing/play.js
+++ Commands/Main/Song/Before-Playing/play.js
@@ -2,7 +2,9 @@ const {StreamType,VoiceConnectionStatus, AudioPlayerStatus, createAudioResource 
 const play = require("play-dl")
 const queueFunc = require("../../../../Imports/queue")
 
-const {MessageEmbed} = require("discord.js")
+play.authorization()
+
+const { Client , MessageEmbed, MessageActionRow, MessageButton, Interaction , Collection} = require('discord.js');
 
 module.exports = {
     name : 'play',
@@ -37,82 +39,98 @@ module.exports = {
 
         message.channel.send(`**Searching...**🔎 \`\`${query}\`\``).catch(()=>{})
 
-        var result = await play.search(query , { limit : 1 })
-        if(result.length == 0) return message.channel.send("Couldn't find any result").catch(()=>{})
-        console.log(result[0].channel)
-        if(result[0].durationInSec > 3600) return message.channel.send("Video selected is longer than ``1 hour``").catch(()=>{})
+        let check = await play.validate(query)
 
-        try{
-            var data = await play.video_info(result[0].url)
-            var stream = await play.stream(result[0].url)
-        }catch(error){
-            console.log("error"+error)
-            client.guilds.cache.get("896070505717727272").channels.cache.get("896070505717727278").send("Koonkesha karetoon khoob bood ye error peyda kardinm, error:\n" +`\`\`\`js\n${error} \`\`\` `).catch(()=>{})
-            return message.channel.send("Something went wrong , this is probably because youre trying to play a song which which requires age verification").catch(()=>{})
-        }
-
-        var audioResource = createAudioResource(stream.stream,{
-            inputType : stream.type,
-            metadata:{
-                messageChannel:message.channel,
-                title: result[0].title,
-                url: result[0].url,
-                thumbnail: result[0].thumbnail.url,
-                guildId: message.guildId,
-                secDuration: result[0].durationInSec,
-                rawDuration: result[0].durationRaw,
-                requestedBy: message.author.username,
-                data: data ,//used for the seek option
-                is_seeked:false,
-                channel:result[0].channel,
-                streamData:stream
-            }
-        })
-
-        var guild_queue = queue.get(message.guildId)
-        if((queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Paused || queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Playing) &&  queue.get(message.guildId).resources.length != 0){
-            var currentAudioRes = connection.state.subscription.player.state.resource
-            var currentTime = new Date().getTime()
-            var timeMusicStarted = queue.get(message.guildId).timeMusicStarted.getTime()
-            var timePassedFromMusic = ((currentTime - timeMusicStarted)/1000).toFixed(0)
-            let estimated = -timePassedFromMusic
-            queue.get(message.guildId).resources.forEach(r=>{
-                estimated += r.metadata.secDuration
-            })
-            if(currentAudioRes.metadata.is_seeked){
-                estimated += currentAudioRes.metadata.seekVal
-            }
-            queue.get(message.guildId).resources.push(audioResource)
-            
-            var embed = new MessageEmbed()
-            .setColor('#00FFFF')
-            .setAuthor(message.author.username , message.author.avatarURL())
-            .setTitle(result[0].title)
-            .setURL(result[0].url)
-            .setThumbnail(result[0].thumbnail.url)
-            .addFields(
-                { name: '**Duration**', value: result[0].durationRaw  , inline :true},
-                { name: '**Estimated time until playing**', value: secToMinSec(estimated) , inline:true },
-                { name: '**Position in queue**', value: (guild_queue.resources.length-1).toString() , inline:true }
-            )
-            .setFooter("By: **" + result[0].channel.name+ "**" , result[0].channel.icon.url)
-            message.channel.send({embeds:[embed]}).catch(()=>{})
-        }else if(queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Idle && queue.get(message.guildId).resources.length == 0){
-            queue.get(message.guildId).resources.push(audioResource)
-            var player = queue.get(message.guildId).audioPlayer
-            player.play(audioResource)
-        }else{
-            message.channel.send("Sorry , something went wrong that caused a queue system crash.We will have to clear your songs in the queue\n. We'll try our best to fix this issue soon...\nThx for you support , Mylo team support").catch(()=>{})
-            console.log(queue.get(message.guildId).audioPlayer)
-            console.log(queue.get(message.guildId).audioPlayer.state.status)
-            console.log(queue.get(message.guildId).resources)
-            try{
-                if(queue.get(message.guildId).resources.length > 0){
-                    queue.get(message.guildId).resources = []
+        switch(check){
+            case "so_track":
+                if(play.is_expired()){
+                    await play.RefreshToken() // This will check if access token has expired or not. If yes, then refresh the token.
                 }
-                queue.get(message.guildId).audioPlayer.stop(true);
-            }catch{}
+                break
+            case "search":
+                var result = await play.search(query , { limit : 1 })
+                if(result.length == 0) return message.channel.send("Couldn't find any result").catch(()=>{})
+                console.log(result[0].channel)
+                if(result[0].durationInSec > 3600) return message.channel.send("Video selected is longer than ``1 hour``").catch(()=>{})
+        
+                try{
+                    var data = await play.video_info(result[0].url)
+                    var stream = await play.stream(result[0].url)
+                }catch(error){
+                    console.log("error"+error)
+                    client.guilds.cache.get("896070505717727272").channels.cache.get("896070505717727278").send("Koonkesha karetoon khoob bood ye error peyda kardinm, error:\n" +`\`\`\`js\n${error} \`\`\` `).catch(()=>{})
+                    return message.channel.send("Something went wrong , this is probably because youre trying to play a song which which requires age verification").catch(()=>{})
+                }
+        
+                var audioResource = createAudioResource(stream.stream,{
+                    inputType : stream.type,
+                    metadata:{
+                        messageChannel:message.channel,
+                        title: result[0].title,
+                        url: result[0].url,
+                        thumbnail: result[0].thumbnail.url,
+                        guildId: message.guildId,
+                        secDuration: result[0].durationInSec,
+                        rawDuration: result[0].durationRaw,
+                        requestedBy: message.author.username,
+                        data: data ,//used for the seek option
+                        is_seeked:false,
+                        channel:result[0].channel,
+                        streamData:stream
+                    }
+                })
+        
+                var guild_queue = queue.get(message.guildId)
+                if((queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Paused || queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Playing) &&  queue.get(message.guildId).resources.length != 0){
+                    var currentAudioRes = connection.state.subscription.player.state.resource
+                    var currentTime = new Date().getTime()
+                    var timeMusicStarted = queue.get(message.guildId).timeMusicStarted.getTime()
+                    var timePassedFromMusic = ((currentTime - timeMusicStarted)/1000).toFixed(0)
+                    let estimated = -timePassedFromMusic
+                    queue.get(message.guildId).resources.forEach(r=>{
+                        estimated += r.metadata.secDuration
+                    })
+                    if(currentAudioRes.metadata.is_seeked){
+                        estimated += currentAudioRes.metadata.seekVal
+                    }
+                    queue.get(message.guildId).resources.push(audioResource)
+                    
+                    var embed = new MessageEmbed()
+                    .setColor('#00FFFF')
+                    .setAuthor(message.author.username , message.author.avatarURL())
+                    .setTitle(result[0].title)
+                    .setURL(result[0].url)
+                    .setThumbnail(result[0].thumbnail.url)
+                    .addFields(
+                        { name: '**Duration**', value: result[0].durationRaw  , inline :true},
+                        { name: '**Estimated time until playing**', value: secToMinSec(estimated) , inline:true },
+                        { name: '**Position in queue**', value: (guild_queue.resources.length-1).toString() , inline:true }
+                    )
+                    .setFooter("By: **" + result[0].channel.name+ "**" , result[0].channel.icon.url)
+                    message.channel.send({embeds:[embed]}).catch(()=>{})
+                }else if(queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Idle && queue.get(message.guildId).resources.length == 0){
+                    queue.get(message.guildId).resources.push(audioResource)
+                    var player = queue.get(message.guildId).audioPlayer
+                    player.play(audioResource)
+                }else{
+                    message.channel.send("Sorry , something went wrong that caused a queue system crash.We will have to clear your songs in the queue\n. We'll try our best to fix this issue soon...\nThx for you support , Mylo team support").catch(()=>{})
+                    console.log(queue.get(message.guildId).audioPlayer)
+                    console.log(queue.get(message.guildId).audioPlayer.state.status)
+                    console.log(queue.get(message.guildId).resources)
+                    try{
+                        if(queue.get(message.guildId).resources.length > 0){
+                            queue.get(message.guildId).resources = []
+                        }
+                        queue.get(message.guildId).audioPlayer.stop(true);
+                    }catch{}
+                }
+            break
+            default:
+                message.channel.send("Please use a valid url")
+            break
         }
+
+
     }
 }
 
