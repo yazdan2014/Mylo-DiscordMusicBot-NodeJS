94335c1c2b0f4aa918e78eb26af4a17630d18536
yazdanzn <>
2021-10-22 21:50:43 +0330

    sdf

--- test.js
+++ test.js
@@ -2,8 +2,9 @@
 const play = require("play-dl")
 
 async function myf(){
-
-let sp_data = await play.spotify("https://open.spotify.com/track/62LJFaYihsdVrrkgUOJC05?si=a286792f65e34ee0")
-console.log(sp_data.artists)}
-
-myf()
+    if(play.is_expired()){
+        await play.refreshToken() // This will check if access token has expired or not. If yes, then refresh the token.
+    }
+    let sp_data = await play.spotify("https://open.spotify.com/track/62LJFaYihsdVrrkgUOJC05?si=a286792f65e34ee0")
+    console.log(sp_data.artists[0].name)
+}
