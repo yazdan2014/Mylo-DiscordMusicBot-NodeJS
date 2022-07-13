bd97a0600a6b0a26bf1be1163b7188bf3e2c33e8
Yaz <yazeinal@gmail.com>
2022-07-13 17:36:46 +0430

    grab

---
+++ Commands/Main/While-Playing/grab.js
@@ -0,0 +1,24 @@
+const {getVoiceConnection , AudioPlayerStatus} = require("@discordjs/voice")
+const {MessageEmbed} = require("discord.js")
+
+module.exports = {
+    name : 'grab',
+    aliases:[],
+    description: 'Sends the current playing song through direct messages',
+    execute(message , client, queue, arg){
+        var connection = getVoiceConnection(message.guildId)
+        if(!connection ) return message.channel.send("Im not in a voice channel").catch(()=>{})
+        if(queue.get(message.guildId).audioPlayer.state.status != AudioPlayerStatus.Playing) return message.channel.send("Nothing is being played").catch(()=>{})
+        var currentAudioRes = connection.state.subscription.player.state.resource
+        if(!currentAudioRes) return message.channel.send("Nothing is being played").catch(()=>{})
+
+        let duration = currentAudioRes.metadata.secDuration
+        var embed = new MessageEmbed()
+            .setColor('#1202F7')
+            .setTitle(currentAudioRes.metadata.title)
+            .setDescription(`Duration: ${duration}`)
+            .setURL(currentAudioRes.metadata.url)
+            .setThumbnail(currentAudioRes.metadata.thumbnail)
+            message.author.dmChannel.send({embeds:[embed]}).catch()
+    }
+}
