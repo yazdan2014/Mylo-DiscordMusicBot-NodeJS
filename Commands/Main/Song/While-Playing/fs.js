3e206a278d56f99e35f0c4df748084be4fdc2a19
Yaz <yazeinal@gmail.com>
2022-07-13 12:14:25 +0430

    force skip

---
+++ Commands/Main/Song/While-Playing/fs.js
@@ -0,0 +1,28 @@
+const {MessageEmbed, MessageActionRow, MessageButton, Interaction , Collection} = require('discord.js');
+const {StreamType,VoiceConnectionStatus, AudioPlayerStatus, createAudioResource ,createAudioPlayer , NoSubscriberBehavior ,joinVoiceChannel , getVoiceConnection, entersState } = require('@discordjs/voice');
+const play = require("play-dl")
+const arraySplitter = require("split-array")
+// const fetch = require('node-fetch');
+
+
+module.exports = {
+    name : 'fs',
+    aliases:["forceskip"],
+    description: 'forceskips the current song',
+    async execute(message , client, queue, arg){
+        if(!message.guild.me.voice.channel) return message.channel.send("Im not in a vc").catch(()=>{})
+        if(message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send("Youre not in the same channel as bot is").catch(()=>{})
+        if(!message.member.roles.cache.some(r=> r.name.toLowerCase() == "dj") && !message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("Only members with the \"DJ\" role or administrator permission can control bot actions").catch(()=>{})
+        var connection = getVoiceConnection(message.guildId)
+        if(queue.get(message.guildId).resources.length > 1){
+            connection.state.subscription.player.stop()
+        }else if(queue.get(message.guildId).resources.length == 1 ){
+            queue.get(message.guildId).loopStatue = false
+            connection.state.subscription.player.stop()
+            message.react("✅").catch(()=>{})
+        }
+        else{
+            message.channel.send("Nothing is being played").catch(()=>{})
+        }
+    }
+}
--- index.js
+++ index.js
@@ -216,24 +216,7 @@ client.on("messageCreate", async message => {
 
     try{
     switch (command) {
-
-        case "skip":case "s":
-        break
         case "fs":case "forceskip":
-            if(!message.guild.me.voice.channel) return message.channel.send("Im not in a vc").catch(()=>{})
-            if(message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send("Youre not in the same channel as bot is").catch(()=>{})
-            if(!message.member.roles.cache.some(r=> r.name.toLowerCase() == "dj") && !message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("Only members with the \"DJ\" role or administrator permission can control bot actions").catch(()=>{})
-            var connection = getVoiceConnection(message.guildId)
-            if(queue.get(message.guildId).resources.length > 1){
-                connection.state.subscription.player.stop()
-            }else if(queue.get(message.guildId).resources.length == 1 ){
-                queue.get(message.guildId).loopStatue = false
-                connection.state.subscription.player.stop()
-                message.react("✅").catch(()=>{})
-            }
-            else{
-                message.channel.send("Nothing is being played").catch(()=>{})
-            }
             break
         case "seek":
             var connection = getVoiceConnection(message.guildId)
