d2ff075e9d6a68c58405f86f70eae7533d90578d
Yaz <yazeinal@gmail.com>
2022-07-13 16:48:22 +0430

    Update clear.js

--- Commands/Main/While-Playing/clear.js
+++ Commands/Main/While-Playing/clear.js
@@ -6,14 +6,14 @@ module.exports = {
         let prefix = '-'
         if (message.channel.messages.cache.size == 0) return null
         if (!message.channel.manageable) return message.channel.send("channel is not manageable for the bot")
-        message.channel.messages.fetch({limit:100}).then(async msgs=>{
-            msgs.forEach(async m =>{
+        message.channel.messages.fetch({limit:100}).then(msgs=>{
+            for(const m of msgs){
                 if((m.content.startsWith(prefix) || m.author.id == client.user.id) && m.deletable){
                     try {
-                        await m.delete()
+                        m.delete()
                     } catch(err){}
                 }
-            })
+            }
             message.channel.send('done! ✅').then(m =>{
                 m.delete({timeout:5000})
             })
