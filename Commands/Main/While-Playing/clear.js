b2889892e5f885d24ff0859e1968b8f29a3a4a4b
Yaz <yazeinal@gmail.com>
2022-07-13 15:51:16 +0430

    Update clear.js

--- Commands/Main/While-Playing/clear.js
+++ Commands/Main/While-Playing/clear.js
@@ -2,7 +2,7 @@ module.exports = {
     name : 'clear',
     aliases:["clean"],
     description: 'Deletes messages and commands on the bot in the channel the command is sent',
-    async execute(message , client, queue, arg){
+    execute(message , client, queue, arg){
         let prefix = '-'
         if (message.channel.messages.cache.size == 0) return null
         if (!message.channel.manageable) return message.channel.send("channel is not manageable for the bot")
