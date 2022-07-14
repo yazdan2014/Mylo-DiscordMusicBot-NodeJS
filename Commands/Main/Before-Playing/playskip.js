f402b946befcff85276b8ce6241b304ee3c4c0ac
Yaz <yazeinal@gmail.com>
2022-07-14 12:49:59 +0430

    Update playskip.js

--- Commands/Main/Before-Playing/playskip.js
+++ Commands/Main/Before-Playing/playskip.js
@@ -13,12 +13,13 @@ module.exports = {
         var channel = message.member.voice.channel
         if(!channel) return message.channel.send("Join a channel").catch(()=>{})
         if(!channel.joinable) return message.channel.send("Bot doesn't have permission to join your voice channel").catch(()=>{})
-        if(!message.guild.me.voice.channel) return message.channel.send("Im not in a vc").catch(()=>{})
-        if(message.member.voice.channel.id !== message.guild.me.voice.channel.id)return message.channel.send("Youre not in the same voice channel as bot is").catch(()=>{})
         if(!message.member.roles.cache.some(r=> r.name.toLowerCase() == "dj") && !message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("Only members with the \"DJ\" role or administrator permission can control bot actions").catch(()=>{})
+        try{
+            if((message.member.voice.channel.id !== message.guild.me.voice.channel.id) && queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Playing) return message.channel.send("Youre not in the same channel as bot is").catch(()=>{})
+        }catch{}
 
-        var query = arg.replaceAll("#", "sharp")
         if(!query) return message.channel.send("Search for an actuall song").catch(()=>{})
+        var query = arg.replaceAll("#", "sharp")
 
         queue.get(message.guildId).resources.splice(1 , queue.get(message.guildId).resources.length + 1)
         fs.execute(message,client,queue,arg)
