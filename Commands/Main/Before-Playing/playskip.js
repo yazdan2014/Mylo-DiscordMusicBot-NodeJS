4dabd783ee35c25b1f48459d8a27e8e72244b767
Yaz <yazeinal@gmail.com>
2022-07-14 12:45:10 +0430

    Update playskip.js

--- Commands/Main/Before-Playing/playskip.js
+++ Commands/Main/Before-Playing/playskip.js
@@ -15,12 +15,10 @@ module.exports = {
         if(!channel.joinable) return message.channel.send("Bot doesn't have permission to join your voice channel").catch(()=>{})
         if(!message.guild.me.voice.channel) return message.channel.send("Im not in a vc").catch(()=>{})
         if(message.member.voice.channel.id !== message.guild.me.voice.channel.id)return message.channel.send("Youre not in the same voice channel as bot is").catch(()=>{})
-        if(queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Idle ) return message.channel.send("Nothing is being played").catch(()=>{})
         if(!message.member.roles.cache.some(r=> r.name.toLowerCase() == "dj") && !message.member.permissions.has("ADMINISTRATOR")) return message.channel.send("Only members with the \"DJ\" role or administrator permission can control bot actions").catch(()=>{})
 
         var query = arg.replaceAll("#", "sharp")
         if(!query) return message.channel.send("Search for an actuall song").catch(()=>{})
-        
 
         queue.get(message.guildId).resources.splice(1 , queue.get(message.guildId).resources.length + 1)
         fs.execute(message,client,queue,arg)
