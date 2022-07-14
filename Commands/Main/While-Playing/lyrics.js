62a0972094dc2552ac2174cae9ee70c08142dea0
Yaz <yazeinal@gmail.com>
2022-07-14 13:13:36 +0430

    Update lyrics.js

---
+++ Commands/Main/While-Playing/lyrics.js
@@ -0,0 +1,26 @@
+const {StreamType,VoiceConnectionStatus, AudioPlayerStatus, createAudioResource ,createAudioPlayer , NoSubscriberBehavior ,joinVoiceChannel , getVoiceConnection, entersState } = require('@discordjs/voice');
+const {MessageEmbed} = require("discord.js")
+const solenolyrics= require("solenolyrics"); 
+
+module.exports = {
+    name : 'lyrics',
+    aliases:["lr"],
+    description: 'Sends an embed message with the current song lyrics',
+    execute(message , client, queue, arg){
+        var channel = message.member.voice.channel
+        if(!channel) return message.channel.send("Join a channel").catch(()=>{})
+        if(!channel.joinable) return message.channel.send("Bot doesn't have permission to join your voice channel").catch(()=>{})
+        if(!message.guild.me.voice.channel) return message.channel.send("Im not in a vc").catch(()=>{})
+        if(message.member.voice.channel.id !== message.guild.me.voice.channel.id)return message.channel.send("Youre not in the same voice channel as bot is").catch(()=>{})
+        if(queue.get(message.guildId).audioPlayer.state.status == AudioPlayerStatus.Idle ) return message.channel.send("Nothing is being played").catch(()=>{})
+
+        let song = connection.state.subscription.player.state.resource.metadata
+        let lyrics = await solenolyrics.requestLyricsFor(song.title)
+        var embed = new MessageEmbed()
+            .setColor('#1202F7')
+            .setAuthor('Lyrics🎵' , client.user.avatarURL())
+            .setTitle(song.title)
+            .setDescription('`'+ lyrics +'`')
+        message.channel.send({embeds:[embed]}).catch(()=>{})
+    }
+}
