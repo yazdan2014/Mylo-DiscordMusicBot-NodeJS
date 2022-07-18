cb3c03a12c3bd6abacab804f88e321bfdc214136
Yaz <yazeinal@gmail.com>
2022-07-18 21:57:49 +0430

    sewsss

---
+++ Commands/Main/While-Playing/replay.js
@@ -0,0 +1,17 @@
+const { Client , MessageEmbed, MessageActionRow, MessageButton, Interaction , Collection} = require('discord.js');
+const {StreamType,VoiceConnectionStatus, AudioPlayerStatus, createAudioResource ,createAudioPlayer , NoSubscriberBehavior ,joinVoiceChannel , getVoiceConnection, entersState } = require('@discordjs/voice');
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES" ] });
+const play = require("play-dl")
+// const fetch = require('node-fetch');
+const replayCom = require("./seek")
+
+client.commands = new Collection()
+const fs = require('fs');
+module.exports = {
+    name : 'replay',
+    aliases:[],
+    description: 'Replays the current song',
+    async execute(message , client, queue, arg){
+        replayCom.execute(message , client, queue, 0)
+    }
+}
